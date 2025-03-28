9c62b31fe5dfc3e32eb34da319f0bb11f6655e05
rod <dev_rodts@outlook.com>
2025-03-28 06:37:48 -0300

    feat: add user responde dto

--- src/main/java/com/devrodts/ecommerce/dtos/UserResponseDTO.java
+++ src/main/java/com/devrodts/ecommerce/dtos/UserResponseDTO.java
@@ -1,4 +1,10 @@
 package com.devrodts.ecommerce.dtos;
+import java.time.LocalDate;
+import java.util.UUID;
 
-public record UserResponse() {
+public record UserResponseDTO() {
+    static UUID userId;
+    static String email;
+    static LocalDate createdAt;
 }
+
